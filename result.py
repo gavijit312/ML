feff9104cff7de91ee6b147c150b1a2ae7ab819c
Avijit Ghosh <gavijit312@gmail.com>
2026-03-27 22:57:47 +0530

    management and fix formatting in result script

--- result.py
+++ result.py
@@ -2,7 +2,7 @@ from sklearn.linear_model import LinearRegression
 import numpy as np
 
 X  = np.array([[1], [2], [3], [4], [5]])
-
+ 
 y = np.array([[40], [45], [50], [55], [60]])
 
 model = LinearRegression()
